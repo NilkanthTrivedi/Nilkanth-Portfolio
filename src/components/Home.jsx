b88618b53122d23341eb9fd366a97c7af534731d
NilkanthTrivedi <nilkanthtrivedi17@gmail.com>
2023-02-09 22:17:38 +0530

    Profile Image fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import HeroImage from "../assets/heroImage.png";
+import HeroImage from "../assets/heroImage.jpeg";
 import { Link } from "react-scroll";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
@@ -11,10 +11,10 @@ export default function Home() {
                 name="home"
                 className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
             >
-                <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+                <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center gap-8 h-full px-4 md:flex-row">
                     <div className="flex flex-col justify-center h-full">
-                        <h2 className="text-4xl sm:text-7xl font-bold text-white">
-                            I'm a Full Stack Developer
+                        <h2 className="text-4xl sm:text-6xl font-bold text-white">
+                            I'm a Frontend Developer
                         </h2>
                         <p className="text-gray-500 py-4 max-w-md">
                             I have 8 years of experience building and desgining
@@ -45,7 +45,7 @@ export default function Home() {
                         <img
                             src={HeroImage}
                             alt="my profile"
-                            className="rounded-2xl mx-auto w-2/3 md:w-full"
+                            className="rounded-full mx-auto w-2/3 md:w-2/3"
                         />
                     </div>
                 </div>
